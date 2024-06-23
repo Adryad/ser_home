ebc1fd8bc6409fe31e831d9dcffdd879f00703f2
Adryad <99326661+Adryad@users.noreply.github.com>
2024-06-23 20:19:56 +0300

    Update views.py

--- yourproject/views.py
+++ yourproject/views.py
@@ -8,30 +8,25 @@ from mqtt_integration.serializers import FaceDataSerializer, NumericalDataSerial
 import subprocess
 import time
 from mqtt_integration.publish import publish_message
-
 import logging
 
 logger = logging.getLogger(__name__)
 class NumericalDataListCreate(generics.ListCreateAPIView):
     queryset = NumericalData.objects.all()
     serializer_class = NumericalDataSerializer
-
     def get(self, request, *args, **kwargs):
         # Run the MQTT subscription command
         process = subprocess.Popen(['python', 'manage.py', 'mqtt_subscribe'])
         
         # Wait for the MQTT subscription to complete and gather some data
         process.wait()  # Wait for the process to complete
-
         # Allow some time for the data to be posted to the API
         time.sleep(5)  # Adjust the sleep time as necessary to ensure data is posted
-
         # Get the latest data from the database
         try:
             latest_data = NumericalData.objects.latest('timestamp')
         except NumericalData.DoesNotExist:
             return JsonResponse({'error': 'No data available'}, status=status.HTTP_404_NOT_FOUND)
-
         # Return the retrieved data
         return JsonResponse({
             'humidity': latest_data.humidity,
@@ -39,11 +34,9 @@ class NumericalDataListCreate(generics.ListCreateAPIView):
             'gas_level': latest_data.gas_level,
             'rain_status': latest_data.rain
         })
-
 class FaceDataListCreate(generics.ListCreateAPIView):
     queryset = FaceData.objects.all()
     serializer_class = FaceDataSerializer
-
 class MQTTAction(generics.CreateAPIView):
     def post(self, request, *args, **kwargs):
         action = request.data.get('action')
@@ -55,13 +48,11 @@ class MQTTAction(generics.CreateAPIView):
 
 class PublishAPIView(APIView):
     def post(self, request, *args, **kwargs):
-        logger.debug("Received data: %s", request.data)  # Debug line
-
+        logger.debug("Received data: %s", request.data) 
         topic = request.data.get('topic')
         status_message = request.data.get('status')
         if not topic or not status_message:
             return Response({'error': 'Topic and status are required'}, status=status.HTTP_400_BAD_REQUEST)
-
         try:
             publish_message(topic, status_message)
             return Response({'message': 'Message published successfully'}, status=status.HTTP_200_OK)
